17ab339a9977ac5d2145cebdef044d6c97e8214f
AyeshW <ayeshweerasinghe.16@cse.mrt.ac.lk>
2022-03-04 10:48:23 +0530

    Remove billing tables

--- choreo-subscriptions/scripts/database/sample_data.sql
+++ choreo-subscriptions/scripts/database/sample_data.sql
@@ -48,25 +48,3 @@ GO
 INSERT INTO choreo_subscriptions_db.dbo.subscription (id,org_id,org_handle,tier_id,billing_date,status) VALUES
 	 (N'0000060f-569d-4394-b689-4624b9a31b5b',N'0000',N'jhondoe',N'01ebea3a-7735-10be-b3c0-ba95f991e877',1627639797657,N'ACTIVE'),
 	 (N'00151c87-07c0-48b5-ad68-103b1f6f1a90',N'1111',N'jackbob',N'01ebea43-be76-1d7a-b410-2d1b873c57af',1627639797657,N'ACTIVE');
-
-INSERT INTO choreo_subscriptions_db.dbo.billing_tier (id,tier_id,product_id,price_id,currency,recurring_interval) VALUES
-	 (N'01ec1491-3eff-1aec-b511-2eec6e3c92d2',N'01ebea3a-7735-10be-b3c0-ba95f991e877',N'prod_K8pD0xG5AqXUzG',N'price_1JY0lgEeYOVsvOhWyxCaQmNy',N'USD',N'month'),
-	 (N'01ec1f83-d773-1a56-87f0-8544854633a9',N'01ec1f8e-7ba6-1f88-bd74-41709200d0c0',N'prod_K9pD0xG5AqXUzG',N'price_3JY0lgEeYOVsvOhWyxCaQmNy',N'USD',N'month'),
-	 (N'01ec1491-316e-1c84-9195-5bbb347a8a0b',N'01ec1d1e-0e9c-16e4-b6c9-1904e9ef9567',N'prod_K7pD0xG5AqXUzG',N'price_2JY0lgEeYOVsvOhWyxCaQmNy',N'USD',N'month'),
-	 (N'01ec1f92-e300-1a7c-b10c-16b408c2e17b',N'01ec1f82-5451-1cfa-83ca-222452b503ab',N'prod_K6pD0xG5AqXUzG',N'price_4JY0lgEeYOVsvOhWyxCaQmNy',N'USD',N'month');
-GO
-
-INSERT INTO choreo_subscriptions_db.dbo.billing_subscription (id,subscription_id,customer_id,stripe_subscription_id,stripe_subscription_item_id) VALUES
-	 (N'01ec148a-c530-1e10-9d57-44bf012eb896',N'0000060f-569d-4394-b689-4624b9a31b5b',N'cus_KCPa1wMr4UJjDX',N'sub_KCPaTebN0yENzY',N'si_KCPa3qnZBTVqO3'),
-	 (N'01ec148a-c367-11d8-b08c-8c01a83304e7',N'00151c87-07c0-48b5-ad68-103b1f6f1a90',N'cus_KCPa1wMr3UJjDX',N'sub_KCPaTebN1yENzY',N'si_KCPa4qnZBTVqO3');
-GO
-
-INSERT INTO choreo_subscriptions_db.dbo.billing_account (id,org_id,customer_id) VALUES
-	 (N'01ec148a-c163-15e4-aaa7-a098f46fe579',N'0000',N'cus_KCPa1wMr4UJjDX'),
-	 (N'01ec148a-c008-1fe6-938c-04fd4734d900',N'1111',N'cus_KCPa1wMr3UJjDX');
-GO
-
-INSERT INTO choreo_subscriptions_db.dbo.billing_history (id,subscribed_user,customer_id,timestamp,operation) VALUES
-	 (N'01ec1491-6d7a-17c8-81df-0c658bb60511',N'Jhon Marcus',N'cus_KCPa1wMr4UJjDX',1627639797657,N'upgrade'),
-	 (N'01ec1490-6a98-1fce-a8b8-41b6ae8735a6',N'Pillip Json',N'cus_KCPa1wMr3UJjDX',1627639797657,N'downgrade');
-GO
